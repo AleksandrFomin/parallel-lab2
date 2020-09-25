852a06cea3dbe8e3e7091a2b9f06a7b7b5ac9f2f
Aleksandr Fomin <fsasha_1997@mail.ru>
2020-09-25 19:09:10 +0300

    commit

--- lab1.c
+++ lab1.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+
+void fill_array(double *arr, int size, double from, double to, unsigned int *seedp)
+{
+	int i;
+	for (i = 0; i < size; i++) {
+		arr[i] = rand_r(seedp) * (to - from) / RAND_MAX + from;
+	}
+}
+
+void print_array(double *arr, int size)
+{
+	int i;
+	printf("arr=[");
+	for (i = 0; i < size; i++) {
+		printf(" %f", arr[i]);
+	}
+	printf("]\n");
+}
+
 int main(int argc, char* argv[])
 {
 	int i, N;
 	struct timeval T1, T2;
 	long delta_ms;
+	double *M1;
+	int A = 540;
+	unsigned int seed;
 
 	N = atoi(argv[1]); /* N равен первому параметру командной строки */
 	gettimeofday(&T1, NULL); /* запомнить текущее время T1 */
 
+	M1 = malloc(sizeof(double) * N);
+	M2 = malloc(sizeof(double) * N / 2);
+
 	for (i = 0; i < 50; i++) /* 50 экспериментов */
 	{
-		srand(i); /* инициализировать начальное значение ГСЧ */
-		/* Заполнить массив исходных данных размером N */
+		seed = i;
+		fill_array(M1, N, 1, A, &seed);
+		fill_array(M2, N, A, 10 * A, &seed);
+
+		// srand(i); /* инициализировать начальное значение ГСЧ */
 		/* Решить поставленную задачу, заполнить массив с результатами
 		*/
 		/* Отсортировать массив с результатами указанным методом */
